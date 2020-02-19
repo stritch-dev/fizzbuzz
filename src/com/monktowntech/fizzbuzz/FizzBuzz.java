9441d09c42a4c6f5770cce93bb48297a462a83d3
stritch-dev <stritch.dev@gmail.com>
2020-02-19 00:14:28 -0600

    All tests passing for Feature 2.

--- src/com/monktowntech/fizzbuzz/FizzBuzz.java
+++ src/com/monktowntech/fizzbuzz/FizzBuzz.java
@@ -1,19 +1,26 @@
 package com.monktowntech.fizzbuzz;
-
 public class FizzBuzz {
 
   public String play(String numberAsString) {
     int number = Integer.parseInt(numberAsString);
-    String result = String.valueOf(number);
-    if (number % 3 == 0 && number % 5 == 0) {
-      result = "fizz_buzz";
+    if (number % 3 == 0
+        && number % 5 == 0
+        && number % 7 == 0) {
+      return "fizz_buzz_pop";
+    } else if (number % 3 == 0 && number % 5 == 0) {
+      return "fizz_buzz";
+    } else if (number % 3 == 0 && number % 7 == 0) {
+      return "fizz_pop";
+    } else if (number % 5 == 0 && number % 7 == 0) {
+      return "buzz_pop";
     } else if (number % 3 == 0) {
-      result = "fizz";
+      return "fizz";
     } else if (number % 5 == 0) {
-      result = "buzz";
+      return "buzz";
+    } else if (number % 7 == 0) {
+      return "pop";
     }
-
-    return result;
+    return numberAsString;
   }
 }
 
--- src/com/monktowntech/fizzbuzz/FizzBuzzTest.java
+++ src/com/monktowntech/fizzbuzz/FizzBuzzTest.java
@@ -8,8 +8,13 @@ class FizzBuzzFeatureOneTest {
   static String FIZZ = "fizz";
   static String BUZZ = "buzz";
   static String FIZZ_BUZZ = "fizz_buzz";
+  static String POP = "pop";
+  static String FIZZ_POP = "fizz_pop";
+  static String BUZZ_POP = "buzz_pop";
+  static String FIZZ_BUZZ_POP = "fizz_buzz_pop";
   FizzBuzz fb = new FizzBuzz();
 
+  // Feature 1
   // numbers
   @Test
   public void _1_returns_1() {
@@ -68,9 +73,70 @@ class FizzBuzzFeatureOneTest {
   public void _45_returns_fizz_buzz() {
     assertEquals(FIZZ_BUZZ, fb.play("45"));
   }
+  // @Test public void _315_returns_fizz_buzz() { assertEquals(FIZZ_BUZZ, fb.play("315")); }
+
+  // Feature 2
+  // pop
+  @Test
+  public void _7_returns_pop() {
+    assertEquals(POP, fb.play("7"));
+  }
 
   @Test
-  public void _315_returns_fizz_buzz() {
-    assertEquals(FIZZ_BUZZ, fb.play("315"));
+  public void _28_returns_pop() {
+    assertEquals(POP, fb.play("28"));
+  }
+
+  @Test
+  public void _77_returns_pop() {
+    assertEquals(POP, fb.play("77"));
+  }
+
+  // fizz pop
+  @Test
+  public void _21_returns_fizz_pop() {
+    assertEquals(FIZZ_POP, fb.play("21"));
+  }
+
+  @Test
+  public void _63_returns_fizz_pop() {
+    assertEquals(FIZZ_POP, fb.play("63"));
+  }
+
+  @Test
+  public void _126_returns_fizz_pop() {
+    assertEquals(FIZZ_POP, fb.play("126"));
+  }
+
+  // buzz pop
+  @Test
+  public void _35_returns_buzz_pop() {
+    assertEquals(BUZZ_POP, fb.play("35"));
+  }
+
+  @Test
+  public void _70_returns_buzz_pop() {
+    assertEquals(BUZZ_POP, fb.play("70"));
+  }
+
+  @Test
+  public void _140_returns_buzz_pop() {
+    assertEquals(BUZZ_POP, fb.play("140"));
+  }
+
+  // fizz buzz pop
+  @Test
+  public void _105_returns_fizz_buzz_pop() {
+    assertEquals(FIZZ_BUZZ_POP, fb.play("105"));
+  }
+
+  @Test
+  public void _210_returns_fizz_buzz_pop() {
+    assertEquals(FIZZ_BUZZ_POP, fb.play("210"));
+  }
+
+  @Test
+  public void _315_returns_fizz_buzz_pop() {
+    assertEquals(FIZZ_BUZZ_POP, fb.play("315"));
   }
 }
