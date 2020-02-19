faf7d57fdd062c8a97d8095607baec2601b47eca
stritch-dev <stritch.dev@gmail.com>
2020-02-18 22:20:55 -0600

    Mosts test passing.

--- src/com/monktowntech/fizzbuzz/FizzBuzz.java
+++ src/com/monktowntech/fizzbuzz/FizzBuzz.java
@@ -5,8 +5,17 @@ public class FizzBuzz {
   public static void main(String[] args) {
   }
 
-  public String play(String number) {
-    return number;
+  public String play(String numberAsString) {
+    int number = Integer.parseInt(numberAsString);
+    String result = String.valueOf(number);
+    if (number % 3 == 0) {
+      result = "fizz";
+    }
+    if (number % 5 == 0) {
+      result = "buzz";
+    }
+
+    return result;
   }
 }
 
