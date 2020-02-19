e49bdaa2f1b50909a157ab4f92b9f5e9dddfe507
stritch-dev <stritch.dev@gmail.com>
2020-02-18 22:24:03 -0600

    All tests passing.

--- src/com/monktowntech/fizzbuzz/FizzBuzz.java
+++ src/com/monktowntech/fizzbuzz/FizzBuzz.java
@@ -2,16 +2,14 @@ package com.monktowntech.fizzbuzz;
 
 public class FizzBuzz {
 
-  public static void main(String[] args) {
-  }
-
   public String play(String numberAsString) {
     int number = Integer.parseInt(numberAsString);
     String result = String.valueOf(number);
-    if (number % 3 == 0) {
+    if (number % 3 == 0 && number % 5 == 0) {
+      result = "fizz_buzz";
+    } else if (number % 3 == 0) {
       result = "fizz";
-    }
-    if (number % 5 == 0) {
+    } else if (number % 5 == 0) {
       result = "buzz";
     }
 
